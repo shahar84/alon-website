b95aa56550dad5214da94fa0b733c92666669ea6
Shahar Polak <shaharp@breezometer.com>
2019-09-30 12:29:57 +0300

    Finish working on the top nav

---
+++ components/top-nav.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"/>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+
+        <v-btn
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          text
+        >
+          {{item.title}}
+        </v-btn>
+
+
+      <v-spacer/>
+      <v-toolbar-title v-text="title"/>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "top-nav",
+    data() {
+        return {
+            clipped: false,
+            drawer: false,
+            fixed: false,
+            items: [
+                {
+                    icon: 'mdi-home',
+                    title: 'דף הבית',
+                    to: '/'
+                },
+                {
+                    icon: 'mdi-information-variant',
+                    title: 'אודות',
+                    to: '/about'
+                },
+                {
+                    icon: 'mdi-chart-bubble',
+                    title: 'הדבר מכרסמים',
+                    to: '/mouse'
+                }, {
+                    icon: 'mdi-chart-bubble',
+                    title: 'ריסוס',
+                    to: '/spraying'
+                },
+
+            ],
+            miniVariant: false,
+            right: true,
+            rightDrawer: false,
+            title: 'אלון המדביר'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -9,28 +9,27 @@ export default {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:400,400i,700&display=swap'}
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
   /*
   ** Nuxt.js dev-modules
   */
@@ -43,19 +42,21 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {
-  },
+  axios: {},
   /*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
   */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: [
+      '~/assets/variables.scss',
+    ],
     rtl: true,
     theme: {
       dark: false,
@@ -72,6 +73,7 @@ export default {
       }
     }
   },
+
   /*
   ** Build configuration
   */
@@ -79,7 +81,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
